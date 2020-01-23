44381ac78a140af4016ba6fde77a9aaef498d943
Arman030598 <57864749+Arman030598@users.noreply.github.com>
2020-01-23 16:32:29 +0400

    'ArmansCommit'

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,20 +5,24 @@ export default ()=>{
     return (
         <div className="SecPortfolio">
             <div className="Skills">
-                <div className={'OurProj'}>
-                    <p className={'OurP'}>OUR</p>
-                    <p className={'proj'}>PROJECTS</p>
+                <div className="opsit">
+                    <div className={'OurProj'}>
+                        <p className={'OurP'}>OUR</p>
+                        <p className={'proj'}>PROJECTS</p>
+                    </div>
+                    <div className={'navSkills'}>
+
+                        <ul>
+                            <li><a href="">HTML</a></li>
+                            <li><a href="">CSS</a></li>
+                            <li><a href="">JS</a></li>
+                            <li><a href="">BOOTSTRAP</a></li>
+                            <li><a href="">BOOTSTRAP</a></li>
+                            <li><a href="">BOOTSTRAP</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className={'navSkills'}>
-                    <ul>
-                        <li><a href="">HTML</a></li>
-                        <li><a href="">CSS</a></li>
-                        <li><a href="">JS</a></li>
-                        <li><a href="">BOOTSTRAP</a></li>
-                        <li><a href="">BOOTSTRAP</a></li>
-                        <li><a href="">BOOTSTRAP</a></li>
-                    </ul>
-                </div>
+
             </div>
             <div className="projectImg"></div>
         </div>
