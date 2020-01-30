5b2d89a67c400e283015927ee8f5be4879ff23a8
Arman030598 <57864749+Arman030598@users.noreply.github.com>
2020-01-30 12:37:26 +0400

    'portfolio'

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,30 +1,58 @@
 import React from "react";
 import './Portfolio.scss'
+import img1 from './PortfolioImg/works1.jpg'
+import img2 from './PortfolioImg/works2.jpg'
+import img3 from './PortfolioImg/works3.jpg'
+import img4 from './PortfolioImg/works4.jpg'
 
-export default ()=>{
-    return (
-        <div className="SecPortfolio">
-            <div className="Skills">
-                <div className="opsit">
-                    <div className={'OurProj'}>
-                        <p className={'OurP'}>OUR</p>
-                        <p className={'proj'}>PROJECTS</p>
-                    </div>
-                    <div className={'navSkills'}>
+//moskovyan 3/2
 
-                        <ul>
-                            <li><a href="">HTML</a></li>
-                            <li><a href="">CSS</a></li>
-                            <li><a href="">JS</a></li>
-                            <li><a href="">BOOTSTRAP</a></li>
-                            <li><a href="">BOOTSTRAP</a></li>
-                            <li><a href="">BOOTSTRAP</a></li>
-                        </ul>
+export default class Portfolio extends React.Component{
+    state = {
+        navBar:[
+            {id:15,name:'SHOW ALL',img:['img1','img2','img3','img4']},
+            {id:0,name:'text1',img:'img1'},
+            {id:1,name:'text2',img:'img2'},
+            {id:2,name:'text3',img:'img3'},
+            {id:3,name:'text4',img:'img4'},
+        ],
+        indexLi:0,
+    }
+
+
+
+
+
+    render() {
+        return (
+            <div className="SecPortfolio">
+                <div className="Skills">
+                    <div className="opsit">
+                        <div className={'OurProj'}>
+                            <p className={'OurP'}>OUR</p>
+                            <p className={'proj'}>PROJECTS</p>
+                        </div>
+                        <div className={'navSkills'}>
+                            <div className={'skilsImg'}>
+
+                            </div>
+                            <div className={'navSkill'}>
+                                <ul>{
+                                    this.state.navBar.map((e,i)=>{
+                                        return <li onClick={()=>this.setState({indexLi:i})}>{e.name}</li>
+                                    })
+                                }
+
+                                </ul>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
-
+                <div className="projectImg">{this.state.indexLi == 0 ?this.state.navBar[this.state.indexLi].img.map(e=>{return e}):this.state.navBar[this.state.indexLi].img}</div>
             </div>
-            <div className="projectImg"></div>
-        </div>
-    )
+        )
+    }
+
+
 }
