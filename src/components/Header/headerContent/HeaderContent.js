ddb235cfe1736607d9cc29caffd6ac6781dcbd36
VaheMartirosyan <vahemartirosyan017@icloud.com>
2020-02-03 17:26:37 +0400

    vol1

--- src/components/Header/headerContent/HeaderContent.js
+++ src/components/Header/headerContent/HeaderContent.js
@@ -8,6 +8,7 @@ export default ()=>{
             <video muted loop="loop" playsInline="true"   autoPlay='autoplay'className="active_pause">
                 <source src={video} type={'video/mp4'}/>
             </video>
+            <h2><span>Alcyon Systems</span> – is a communication group, which provides clients with business results in a modern digital context.</h2>
         </div>
     )
 }
