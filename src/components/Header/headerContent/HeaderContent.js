25e5f10b6da44a0e0615ca438cce76b45c3920fa
VaheMartirosyan <vahemartirosyan017@icloud.com>
2020-02-06 13:30:09 +0400

    ServicesUpdate

--- src/components/Header/headerContent/HeaderContent.js
+++ src/components/Header/headerContent/HeaderContent.js
@@ -5,7 +5,7 @@ import './HeaderContent.scss'
 export default ()=>{
     return(
         <div className='video_div'>
-            <video muted loop="loop" playsInline="true"   autoPlay='autoplay' className="active_pause">
+            <video muted loop="loop"    autoPlay='autoplay' className="active_pause">
                 <source src={video} type={'video/mp4'} className='header_source'/>
             </video>
             <h2><span>Alcyon Systems</span> – is a communication group, which provides clients with business results in a modern digital context.</h2>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,22 +1,77 @@
 import React,{Component} from "react";
-import './Services.scss'
-import video from './backgroundVideo/services.mp4';
+import './Services.scss';
+import video from './backgroundVideo/services.gif';
 
 export default class Services extends Component{
     constructor(props){
         super(props);
         this.state = {
-            isHovered: false
+            isHovered: false,
+            titles:['Web Development','Web Design','Ecommerce','QA Testing','Web Maintenance','Responsive Design','SMM'],
+            text:[
+                {
+                    content:'It\'s difficult to imagine a successful business without a good website,' +
+                        ' which will best describe your company\'s what, how, why and who. ' +
+                        'We are Alcyon team and if you want a web based on unique solutions,' +
+                        ' you are in the right place.',
+                    hoverColor:'red',
+                },
+                {
+                    content:'If you want to attract more customers to your website, you will need solid design to ' +
+                        'reach your goal. Our creative designers will breathe life into your website.',
+                    hoverColor:'green',
+                },
+                {
+                    content:'Day by day the ecommerce growth reinforces, which means that there is still' +
+                        ' endless opportunity for brands to start-up an ecommerce website and maximise its earnings.' +
+                        ' With our ecommerce service you can have the key to the successful online sales.',
+                    hoverColor:'blue',
+                },
+                {
+                    content:'Another vital role of complete website is QA testing.' +
+                        ' It is meant to increase customer confidence and a company\'s credibility, ' +
+                        'while also improving work processes and efficiency. ' +
+                        'After the testing you can be ensured that it works without any errors and can go live.',
+                    hoverColor:'white',
+                },
+                {
+                    content:'Updating and adding new content to your website will improve its ' +
+                        'SEO performance and increase your return on investment. We can take care of your ' +
+                        'website maintenance, protection and even content updates! ' +
+                        'If you don’t have time or don’t know how to maintain your website, we are here for you.',
+                    hoverColor:'orange',
+                },
+                {
+                    content:'Do you know that 63% of web traffic is coming from mobile devices ? It means that if you don`t ' +
+                        'have well structured mobile friendly website, then you have a big chance to lose real customers. ' +
+                        'With our qualified developers team you`ll ' +
+                        'get fast, professional and mobile friendly website which your customers will adore.',
+                    hoverColor:'pink',
+                },
+                {
+                    content:'The more specific your strategy is, ' +
+                        'the more effective the execution will be. Smart social strategies ' +
+                        'are several times more likely to seal a deal․ Do not hesitate to contact us.',
+                    hoverColor:'gold',
+                },
+            ],
+            e:'',
+            a:''
+
         };
-        this.handleHover = this.handleHover.bind(this);
-        this.hoverOver = this.hoverOver.bind(this)
+
+
     }
-    handleHover(){
+    handleHover=(e)=>{
         this.setState({
-            isHovered: true
+            isHovered: true,
+            e:this.state.text[e].content,
+            a:this.state.text[e].hoverColor
         });
+
     }
-    hoverOver(){
+    hoverOver=()=>{
+
         this.setState({
             isHovered: false
         });
@@ -25,28 +80,40 @@ export default class Services extends Component{
     render() {
         const btnClass = this.state.isHovered ? "hoverMade" : "hoverStart";
         return(
-            <div className={'services'}>
-                <video muted   loop="loop" playsinline="true" autoPlay='autoplay' className="services_video">
-                    <source src={video} type={'video/mp4'} className='header_source'/>
-                </video>
-                <div className={'li d-flex flex-wrap'}>
-                    <div className={'col-lg-6 services_intro'}>
+            <div className={'services d-flex flex-wrap'} style={{backgroundImage:`url(${video})`}}>
+
+                <div className={'services_content col-lg-6' } >
+                    <div className={'services_text'}>
                         <h2>Services</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Alias autem dolore dolores ea enim fugiat incidunt
-                            ipsa iure, iusto minus molestias necessitatibus,
-                            nulla obcaecati quae ratione similique, veniam voluptatibus. Qui.
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aperiam debitis ea enim,
+                            et exercitationem fuga harum id illo laudantium maxime minima odit perferendis possimus
+                            provident ratione repudiandae sunt, ullam!
                         </p>
-                        <p className={btnClass}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eligendi.</p>
                     </div>
-                    <div className={'services_list col-lg-6 '}>
-                        <div className={'list_item'} onMouseEnter={this.handleHover} onMouseLeave={this.hoverOver}>
-                            <h2>Web Development</h2>
+
+                        <div style={{background:this.state.a}} className={`${btnClass} hoverPos`}>
+                                 <p >{this.state.e}</p>
                         </div>
-                    </div>
                 </div>
+                <div className={'col-lg-6 services_list'} >
+                    {this.state.titles.map((e,i)=>{
+                                return(
+                                     <div key={i} className={'list_item'} onMouseEnter={this.handleHover.bind(this,i)} onMouseLeave={this.hoverOver}>
+                                            <h2 style={{color:this.state.a}}>{e}</h2>
+                                     </div>
+                                )})}
+
+
+                </div>
+
             </div>
         )
     }
 }
+
+//
+//
+
+
+//
+//
