2e26494ee5871c2ba023df144c96b4dac79c4ea6
VaheMartirosyan <vahemartirosyan017@icloud.com>
2020-03-06 11:38:47 +0400

    mediaThings

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -9,103 +9,132 @@ export default class Slider extends Component {
                 <div className="slider ">
                     <div className="slide-track ">
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://arssystems.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners1_kvybwr.png"
+                                    height="100"
+                                    width="100"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://colibrilab.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners2_rhc0vn.png"
+                                    height="120"
+                                    width="200"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://arnology.am/home" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners3_emdryk.png"
+                                    height="100"
+                                    width="100"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                             <a href="http://profitdeco.com/" target="_blank" rel="noopener noreferrer">
+                                 <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476335/AlcyonSystems/Alcyon%20Partners/partners4_vfsb6c.jpg"
+                                    height="100"
+                                    width="200"
+                                    alt=""/>
+                             </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://arssystems.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners1_kvybwr.png"
+                                    height="100"
+                                    width="100"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://colibrilab.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners2_rhc0vn.png"
+                                    height="120"
+                                    width="200"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://arnology.am/home" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners3_emdryk.png"
+                                    height="100"
+                                    width="100"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://profitdeco.com/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476335/AlcyonSystems/Alcyon%20Partners/partners4_vfsb6c.jpg"
+                                    height="100"
+                                    width="200"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://arssystems.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners1_kvybwr.png"
+                                    height="100"
+                                    width="100"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://colibrilab.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners2_rhc0vn.png"
+                                    height="120"
+                                    width="200"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://arnology.am/home" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners3_emdryk.png"
+                                    height="100"
+                                    width="100"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://profitdeco.com/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476335/AlcyonSystems/Alcyon%20Partners/partners4_vfsb6c.jpg"
+                                    height="100"
+                                    width="200"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://arssystems.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners1_kvybwr.png"
+                                    height="100"
+                                    width="100"
+                                    alt=""/>
+                            </a>
                         </div>
                         <div className="slide">
-                            <img
-                                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
-                                height="100"
-                                width="250"
-                                alt=""/>
+                            <a href="http://colibrilab.am/" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src="https://res.cloudinary.com/alcyonsystems/image/upload/v1583476207/AlcyonSystems/Alcyon%20Partners/partners2_rhc0vn.png"
+                                    height="120"
+                                    width="200"
+                                    alt=""/>
+                            </a>
                         </div>
+
                     </div>
                 </div>
             </div>
