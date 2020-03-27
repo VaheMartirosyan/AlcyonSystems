5b45fea7b3d63a8f98e0be1e91cde5b8ff17a176
HrachCode <virap1000@mail.ru>
2020-03-27 20:05:57 +0400

    156465846

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const port = process.env.PORT || 5000
 const cors = require('cors')
 const morgan = require('morgan')
-const emailer = require('./routs/emailer')
+
 const transporter = require('./control/nodemailer')
 
 
@@ -31,8 +31,6 @@ app.use(express.urlencoded({
 
 
 
- 
-
 app.post('/contact',(req,res)=>{
   const  mailOptions = {
    
@@ -42,7 +40,7 @@ app.post('/contact',(req,res)=>{
         <h2>user name is ${req.body.name}</h2>.</br> 
         <h3>user phone ${req.body.phone} </h3>
        <h4> ${req.body.name} email  ${req.body.email}</h4>
-         <p>${req.body.name} message ${req.body.description}</p>`
+         <p>${req.body.name} message : ${req.body.description}</p>`
   };
 
   transporter.sendMail(mailOptions, function(error, info){
@@ -56,8 +54,32 @@ app.post('/contact',(req,res)=>{
    });
   
 }) 
-app.post('/angularjs',(req,res)=>{
+app.post('/cariers',(req,res)=>{
+  const user = req.body.message;
+  
+  
+  const  mailOptions = {
+   
+    to:'virap100@mail.ru',
+    subject: req.body.name,
+    html:`<h1>from cariers</h1>
+        <h2>user name is ${user.name}</h2>.</br> 
+        <h3>user phone ${user.phone} </h3>
+       <h4> ${user.name} email  ${user.email}</h4>
+       <h4> ${user.name} repository link  ${user.link}</h4>
+         <p>${user.name} message :${user.description}</p>`
+  };
 
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      return res.json({err:error})
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).json({message:'mail sended succssefuly'});
+    }
+   });
+  
 })
 
 app.post('/phplaravel',(req,res)=>{
